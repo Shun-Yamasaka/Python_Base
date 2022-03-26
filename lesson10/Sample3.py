7b392cb4f16f2b96c61a4d42e92f605580b74df9
shun yamasaka <118syuunico@gmail.com>
2022-03-26 16:17:50 +0900

    CSVファイル読み込みサンプル

---
+++ lesson10/Sample3.py
@@ -0,0 +1,12 @@
+import csv # csvモジュールをインポート
+
+f = open("Sample.csv", "r") # csvファイルを読み込む
+
+rd = csv.reader(f) # リーダーを取得
+
+for row in rd: # 1行取り出し
+    for col in row: # その中の列項目1つを取り出し
+        print(col, end="") # 表示
+    print()
+f.close() # ファイルクローズ
+
